3e68b17913157409472e9f1fad17b7dc172ab6cb
Robert Reilly <robert.d.reilly@hotmail.com>
2023-07-05 10:42:21 +0800

    drunkendodgergame

---
+++ Assets/Scripts/DrunkenDodger/ObjectMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+
+    [Header("ObjectMovementSettings")]
+
+    [SerializeField] private float minSpeed = 2f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float finalScale = 2f;
+    [SerializeField] private float initialScale = 0.5f;
+    [SerializeField] private float maxHeight = 10f;
+
+    
+    private Vector3 moveDirection;
+    private Transform playerTransform;
+
+    void Start()
+    {      
+        moveSpeed = Random.Range(minSpeed, maxSpeed);       
+        moveDirection = new Vector3(Random.Range(-1f, 1f), -1f, 0f).normalized;      
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;       
+        transform.localScale = new Vector3(initialScale, initialScale, 1f);
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = moveDirection * moveSpeed;
+
+        if (transform.position.y < -10f)
+        {
+            Destroy(gameObject);
+        }
+              
+        float distanceToPlayer = Mathf.Abs(transform.position.y - playerTransform.position.y);
+        float t = Mathf.Clamp01(distanceToPlayer / maxHeight);
+        float scale = Mathf.Lerp(finalScale, initialScale, t);  
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
---
+++ Assets/Scripts/DrunkenDodger/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float characterWidth;
+
+    [Header("Screen Settings")]
+    [SerializeField] private float leftScreenBorder;
+    [SerializeField] private float rightScreenBorder;
+
+    private void Start()
+    {
+        CalculateScreenBorders();
+    }
+
+    private void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        float newPositionX = transform.position.x + moveInput * moveSpeed * Time.deltaTime;
+        float clampedX = Mathf.Clamp(newPositionX, leftScreenBorder, rightScreenBorder);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+
+    private void CalculateScreenBorders()
+    {
+        float screenRatio = (float)Screen.width / Screen.height;
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        float cameraWidth = cameraHeight * screenRatio;
+
+        characterWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        leftScreenBorder = Camera.main.transform.position.x - cameraWidth / 2 + characterWidth / 2;
+        rightScreenBorder = Camera.main.transform.position.x + cameraWidth / 2 - characterWidth / 2;
+    }
+}
