e126e263ffd34da4baa222b4daa530e1814218e6
Alessandra <gwensummer@yahoo.com.au>
2023-07-13 17:19:53 +0800

    Shaking Microgame Fixed

--- Assets/Scripts/ShakingMicrogameScript.cs
+++ Assets/Scripts/ShakingMicrogameScript.cs
@@ -1,40 +1,63 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Timers;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ShakingMicrogameScript : MonoBehaviour
 {
-
-    public float mashDelay = 0.5f;
-    public GameObject text;
+    // Change depending on UI Manager
+    public GameObject instruction;
     public int counter;
+    public float timer;
+    private float _timer;
+    
+    // Fixed 
+    public float mashDelay = 0.5f;
     public GameObject shaker;
+    public GameObject timerbar;
+    private float scalextb;
+    
+    //[SerializeField] Text counterUI;
 
-    float mash;
+    private float mash; 
     bool pressed;
-    bool started;
+    float shakesToWin = 30;
+
+    bool callOnce;
 
     // Start is called before the first frame update
     void Start()
     {
+        scalextb = timerbar.transform.localScale.x;
+
         mash = mashDelay;
-        text.SetActive(false);
+        _timer = timer;
+
+        //Countdown();
+
+        // Change later depending on UI
+        instruction.SetActive(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            started = true;
-        }
+        GameStart();
+    }
 
-        if(started)
+    public void GameStart()
+    {
+        if (!callOnce)
         {
-            text.SetActive(true);
+            timerbar.transform.localScale = new Vector2(scalextb * (_timer / timer), timerbar.transform.localScale.y);
             mash -= Time.deltaTime;
+            _timer -= Time.deltaTime;
 
             if (Input.GetKeyDown(KeyCode.Space) && !pressed)
             {
@@ -47,6 +70,9 @@ public class ShakingMicrogameScript : MonoBehaviour
                 pressed = false;
                 counter++;
 
+                // Change later depending on UI
+                instruction.SetActive(false);
+
                 if (counter % 2 == 0)
                 {
                     shaker.transform.position = new Vector3(-1.32f, -0.8f, 0.0f);
@@ -54,16 +80,50 @@ public class ShakingMicrogameScript : MonoBehaviour
 
                 else
                 {
-                    shaker.transform.position = new Vector3(1.5f, 1.3f, 0.0f);
+                    shaker.transform.position = new Vector3(1.5f, 1.3f, 1.0f);
                 }
             }
 
-            if (mash <=0)
+            if (counter == shakesToWin)
             {
-                text.GetComponent<Text>().text = "FAILED";
-                Debug.Log("Microgame failed");
+                WinCondition();
+            }
+
+            if (_timer <= 0)
+            {
+                if (counter < 30)
+                {
+                    LoseCondition();
+                }
+                else
+                {
+                    WinCondition();
+                }
+
+                _timer = 0;
+                callOnce = true;
             }
         }
     }
+    public void WinCondition()
+    {
+        instruction.GetComponent<Text>().text = "Win";
+        instruction.SetActive(true);
+        shaker.SetActive(false);
+        Debug.Log("Player Succeeded");
+        instruction.transform.position = new Vector3(0f, 0f, 0f);
+        // Insert positive game end here to return to UI and Game Manager
+        return;
+    }
 
+    public void LoseCondition()
+    {
+        instruction.GetComponent<Text>().text = "FAILED";
+        instruction.SetActive(true);
+        shaker.SetActive(false);
+        Debug.Log("Player Failed");
+        instruction.transform.position = new Vector3(0f, 0f, 0f);
+        // Insert negative game end here to return to UI and Game Manager
+        return;
+    }
 }
