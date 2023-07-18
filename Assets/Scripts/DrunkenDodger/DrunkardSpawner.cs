262d7170b0fe0d4c8608f7315defb9ab09f61fd0
rdreilly217 <61007868+rdreilly217@users.noreply.github.com>
2023-07-18 23:17:54 +0800

    updated drunken dodger

--- Assets/Scripts/DrunkenDodger/DrunkardSpawner.cs
+++ Assets/Scripts/DrunkenDodger/DrunkardSpawner.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
 
 public class DrunkardSpawner : MonoBehaviour
 {
-    [Header("SpawnSettings")]
-    public GameObject objectPrefab;
+    public GameObject[] objectPrefabs;
     public float spawnInterval = 1f;
     public float spawnIncreaseRate = 0.1f;
     public float spawnRange = 3f;
-
+    public float initialScale = 2f;
 
     private float spawnTimer = 0f;
     private bool isGameOver = false;
@@ -32,8 +34,19 @@ public class DrunkardSpawner : MonoBehaviour
 
     void SpawnObject()
     {
-        Vector3 randomPosition = transform.position + new Vector3(Random.Range(-spawnRange, spawnRange), 0f, 0f);
-        GameObject spawnedObject = Instantiate(objectPrefab, randomPosition, Quaternion.identity);
+        GameObject prefab = objectPrefabs[UnityEngine.Random.Range(0, objectPrefabs.Length)];
+        float spawnX = UnityEngine.Random.Range(-spawnRange, spawnRange);
+        float spawnY = Camera.main.orthographicSize + 1f;
+        Vector3 randomPosition = new Vector3(spawnX, spawnY, 0f);
+
+        GameObject spawnedObject = Instantiate(prefab, randomPosition, Quaternion.identity);
+
+        // Set the scale of the spawned object
+        ObjectMovement objectMovement = spawnedObject.GetComponent<ObjectMovement>();
+        if (objectMovement != null)
+        {
+            objectMovement.initialScale = initialScale;
+        }
     }
 
     public void SetGameOver(bool gameOver)
